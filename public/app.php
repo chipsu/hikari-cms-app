53916e78c5f9c93824f2652e1269bab1843ade7f
chips <chips@metrica.se>
2014-11-08 23:50:33 +0100

    app run

--- public/app.php
+++ public/app.php
@@ -2,7 +2,7 @@
 
 require(__DIR__ . '/../lib/hikari/autoload.php');
 
-\hikari\bootstrap\Bootstrap::app([
+\hikari\bootstrap\Bootstrap::run([
 	'path' => __DIR__ . '/../app',
 	'publicPath' => __DIR__,
 ]);
